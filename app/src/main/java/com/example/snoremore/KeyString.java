77845d273c78f2b64087fa36d5f5c0f0e4062b37
Yerline Herrera <63979928+yerlineUCI@users.noreply.github.com>
2021-09-22 15:49:12 -0700

    Create KeyString.java

---
+++ app/src/main/java/com/example/snoremore/KeyString.java
@@ -0,0 +1,39 @@
+package com.example.snoremore;
+
+import android.os.Bundle;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.StrictMode;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+
+public class KeyString  {
+    public static String PREFERENCE_NAME ="k1";
+    public static String PREFERENCE_WAKE ="k2";
+    public static String PREFERENCE_SLEEP ="k3";
+    public static String PREFERENCE_DATE ="k4";
+    public static String PREFERENCE_SUNRISE ="k5";
+    public static String PREFERENCE_SUNSET ="k6";
+    public static String PREFERENCE_GOAL ="k7";
+    public static String PREFERENCE_HOUR ="k8";
+    public static String PREFERENCE_AGE ="k9";
+    public static String PREFERENCE_CALORIES = "k10";
+    public static String PREFERENCE_DISTANCE = "k11";
+    public static String PREFERENCE_GAIN = "k12";
+    public static String PREFERENCE_LOSE = "k13";
+    public static String PREFERENCE_MAINTAIN = "k14";
+}
