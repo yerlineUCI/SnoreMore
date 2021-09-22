fc0f29ce295549518bae4a5b243e49e149fa246b
Yerline Herrera <63979928+yerlineUCI@users.noreply.github.com>
2021-09-22 15:47:24 -0700

    Create FifthFragment.java

---
+++ app/src/main/java/com/example/snoremore/FifthFragment.java
@@ -0,0 +1,31 @@
+package com.example.snoremore;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.navigation.fragment.NavHostFragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import java.security.PublicKey;
+
+public class FifthFragment extends FragmentActivity {
+//    @Override
+//    public void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        //Initiate map fragment
+//        Fragment fragment = new Maps_Fragment();
+//        //Open Fragment
+//        getSupportFragmentManager()
+//                .beginTransaction()
+//                .replace(R.id.frame_layout,fragment)
+//                .commit();
+    }
