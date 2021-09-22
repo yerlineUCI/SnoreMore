bfee56fe4550833cee59c12c4c76c504d3e68ec8
Yerline Herrera <63979928+yerlineUCI@users.noreply.github.com>
2021-09-22 15:48:50 -0700

    Create Hours.java

---
+++ app/src/main/java/com/example/snoremore/Hours.java
@@ -0,0 +1,32 @@
+package com.example.snoremore;
+import java.util.*;
+
+public class Hours {
+    public static void main(String args[]) {
+        Vector<String> intervals = new Vector<String>(24);
+        intervals.addElement("1-2");
+        intervals.addElement("2-3");
+        intervals.addElement("3-4");
+        intervals.addElement("4-5");
+        intervals.addElement("5-6");
+        intervals.addElement("6-7");
+        intervals.addElement("7-8");
+        intervals.addElement("8-9");
+        intervals.addElement("9-10");
+        intervals.addElement("10-11");
+        intervals.addElement("11-12");
+        intervals.addElement("12-13");
+        intervals.addElement("13-14");
+        intervals.addElement("14-15");
+        intervals.addElement("15-16");
+        intervals.addElement("16-17");
+        intervals.addElement("17-18");
+        intervals.addElement("18-19");
+        intervals.addElement("19-20");
+        intervals.addElement("20-21");
+        intervals.addElement("21-22");
+        intervals.addElement("22-23");
+        intervals.addElement("23-24");
+        intervals.addElement("24-1");
+    }
+}
