59bc43bca358eaf87c29770a640de4ff6f5c148a
Yerline Herrera <63979928+yerlineUCI@users.noreply.github.com>
2021-09-22 15:52:59 -0700

    Create SixthFragment.java

---
+++ app/src/main/java/com/example/snoremore/SixthFragment.java
@@ -0,0 +1,13 @@
+package com.example.snoremore;
+
+import androidx.fragment.app.Fragment;
+
+import java.time.ZonedDateTime;
+import java.util.Date;
+import java.util.Calendar;
+
+public class SixthFragment extends Fragment {
+    Date currentTime = Calendar.getInstance().getTime();
+    public SixthFragment() {
+    }
+}
